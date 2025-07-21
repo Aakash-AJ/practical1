28a52d77f6b6779f25363a2c5257b7a64bc62d19
Aakash-AJ <jaiswarakash9833@gmail.com>
2025-07-21 19:51:07 +0530

    changes

--- create_database.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="mypassword"
-)
-mycursor = mydb.cursor()
-mycursor.execute("SHOW DATABASES")
-
